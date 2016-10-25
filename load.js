9b4c8af9e3d0c6a0be59607323bda038c149615c
Johan Sundman <johan00sundman@gmail.com>
2016-10-25 11:00:26 +0200

    Version 9.0

--- load.js
+++ load.js
@@ -77,7 +77,7 @@ var player = {
 		delay: 0.15, // Seconds
 		duration: 0.3, // Seconds
 		color: "purple",
-		damage: 10,
+		damage: 20,
 		last: 0 // Last time a projectile was shot
 		
 		
@@ -124,7 +124,7 @@ var enemy_1 = {
 		delay: 0.8, // Seconds
 		duration: 0.5, // Seconds
 		color: "red",
-		damage: 20,
+		damage: 10,
 		last: 0 // Last time a projectile was shot
 		
 	},
@@ -132,6 +132,55 @@ var enemy_1 = {
 	instruction : {
 		
 		attackDist: 500
+		
+	}
+	
+};
+var enemy_2 = {
+	
+	id: "enemy2",
+	
+	width: 60,
+	height: 60,
+	color: "yellow",
+	
+	health: {
+		
+		state: 150,
+		max: 150,
+		
+		color: "red"
+		
+	},
+	
+	velocity: {
+		
+		x: 0,
+		y: 0,
+		max: 4,
+		min: 0.5,
+		inc: 0.2,
+	    friction: 0.2
+		
+	},
+	
+	projectile: {
+		
+		amount: 20, // 1, 3, 5 ... odd numbers
+		offsetStartTimes: 1, // x >= 1 // NOT 0 = STACKED SHOOTS
+		offsetAngle: 18, // Deg
+		speed: 1.2,
+		delay: 1.0, // Seconds
+		duration: 0.40, // Seconds
+		color: "red",
+		damage: 5,
+		last: 0 // Last time a projectile was shot
+		
+	},
+	
+	instruction : {
+		
+		attackDist: 200
 		
 	}
 	
